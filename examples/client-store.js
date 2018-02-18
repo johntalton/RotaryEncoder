85decffe75e710c103502b8de4616c85eefd2530
johntalton <johntalton@gmail.com>
2018-02-18 06:52:35 +0000

    break out store for readability

---
+++ examples/client-store.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const mqtt = require('mqtt');
+
+class Store {
+  static make(config) {
+    console.log('setup store ', config.url);
+    if(config.url === undefined) { throw Error('undefined mqtt url'); }
+
+    const client = mqtt.connect(config.url, { reconnectPeriod: config.reconnectMSecs });
+    client.on('connect', () => { console.log('mqtt connected'); });
+    client.on('reconnect', () => { });
+    client.on('close', () => { });
+    client.on('offline', () => { console.log('mqtt offline'); });
+    client.on('error', (error) => { console.log(error); process.exit(-1); });
+
+    return client;
+  }
+
+  static sendKnobEvent(client, encoder, event, delta) {
+    if(client.connected) {
+      const topic = 'rotaryEncoder/' + encoder.name;
+      const msg = {
+        name: encoder.name,
+        event: event,
+        delta: delta,
+        estValue: encoder.client.value
+      };
+      console.log('publish: ', topic, msg);
+      client.publish(topic, JSON.stringify(msg), {}, err => {});
+    }
+    else {
+      console.log('not connected, cache value', encoder.name,  encoder.client.value)
+    }
+  }
+
+  static sendButtonEvent(client, encoder, event) {
+    if(client.connected) {
+      const topic = 'rotaryEncoder/' + encoder.name;
+      const msg = {
+        name: encoder.name,
+        event: event,
+        estValue: encoder.client.value
+      };
+      console.log('publish: ', topic, msg);
+      client.publish(topic, JSON.stringify(msg), {}, err => {});
+    }
+    else {
+      console.log('not connected, button event dropped', encoder.name);
+    }
+  }
+}
+
+module.exports = Store;
--- examples/client.js
+++ examples/client.js
@@ -1,80 +1,41 @@
 "use strict";
 
-const mqtt = require('mqtt');
-
 const Config = require('./client-config.js');
 const RotaryEncoder = require('../src/rotaryencoder.js');
+const Store = require('./client-store.js');
 
 function setupEncoders(config) {
-  config.encoders.forEach(encoder => {
-    console.log('build encoder: ' + encoder.name, encoder);
-    encoder.client = RotaryEncoder.make(encoder);
-
-    function cury_handler(event, delta) {
-      return function() {
-        // updated est value even when not connected
-        encoder.client.value += delta;
-        
-        if(config.mqtt.client.connected) {
-          const topic = 'rotaryEncoder/' + encoder.name;
-          const msg = {
-            name: encoder.name,
-            event: event,
-            delta: delta,
-            estValue: encoder.client.value
-          };
-          console.log('publish: ', topic, msg);
-          config.mqtt.client.publish(topic, JSON.stringify(msg), {}, err => {});
-        } 
-        else {
-          console.log('not connected, cache value', encoder.name,  encoder.client.value)
-        }
-      }
-    }
-
-    function bhandler(event) {
-      return () => {
-        if(config.mqtt.client.connected) {
-          const topic = 'rotaryEncoder/' + encoder.name;
-          const msg = {
-            name: encoder.name,
-            event: event,
-            estValue: encoder.client.value
-          };
-          console.log('publish: ', topic, msg);
-          config.mqtt.client.publish(topic, JSON.stringify(msg), {}, err => {});
-        }
-      }
-    }
-
-    RotaryEncoder.buttonUp(encoder.client, bhandler('UP'));
-    RotaryEncoder.buttonDown(encoder.client, bhandler('DOWN'));
-
-    RotaryEncoder.cw(encoder.client, cury_handler('CW', 1));
-    RotaryEncoder.ccw(encoder.client, cury_handler('CCW', -1));
-    RotaryEncoder.rcw(encoder.client, cury_handler('RCW', 1));
-    RotaryEncoder.rccw(encoder.client, cury_handler('RCCW', -1));
-  });
-
+  config.encoders.forEach(encoder => setupEncoder(config, encoder));
   return config;
 }
 
-function setupStore(config) {
-  console.log('setup store ', config.mqtt.url);
-  if(config.mqtt.url === undefined) { return Promise.reject('undefined mqtt url'); }
-  
-  config.mqtt.client = mqtt.connect(config.mqtt.url, { reconnectPeriod: config.mqtt.reconnectMSecs });
-  config.mqtt.client.on('connect', () => { console.log('mqtt connected'); });
-  config.mqtt.client.on('reconnect', () => { });
-  config.mqtt.client.on('close', () => { });
-  config.mqtt.client.on('offline', () => { console.log('mqtt offline'); });
-  config.mqtt.client.on('error', (error) => { console.log(error); process.exit(-1); });
+function setupEncoder(config, encoder) {
+  encoder.client = RotaryEncoder.make(encoder);
 
-  return Promise.resolve(config);
+  function cury_handler(event, delta) {
+    return function() {
+      encoder.client.value += delta;
+      Store.sendKnobEvent(config.mqtt.client, encoder, event, delta);
+    }
+  }
+
+  RotaryEncoder.cw(encoder.client, cury_handler('CW', 1));
+  RotaryEncoder.ccw(encoder.client, cury_handler('CCW', -1));
+  RotaryEncoder.rcw(encoder.client, cury_handler('RCW', 1));
+  RotaryEncoder.rccw(encoder.client, cury_handler('RCCW', -1));
+  
+  function bhandler(event) {
+    return function() {
+      Store.sendButtonEvent(config.mqtt.client, encoder, event)
+    }
+  }
+
+  RotaryEncoder.buttonUp(encoder.client, bhandler('UP'));
+  RotaryEncoder.buttonDown(encoder.client, bhandler('DOWN'));
 }
 
 Config.config('./client.json')
-  .then(setupStore)
+  .then(config => { config.mqtt.client = Store.make(config.mqtt); return config; })
   .then(setupEncoders)
   .then(() => { console.log('Up.'); })
   .catch(e => {
